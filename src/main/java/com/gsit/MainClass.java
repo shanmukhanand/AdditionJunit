f0c692dff187f8e527f3d757a5cd68600de9d4ce
shan sb <shan.sb@outlook.com>
2018-03-23 20:13:30 +0530

    first commit

---
+++ src/main/java/com/gsit/MainClass.java
@@ -0,0 +1,14 @@
+package com.gsit;
+
+
+public class MainClass {
+
+    public static void main(String[] args){
+        System.out.println(" ");
+    }
+
+    public int addition(int a ,int b){
+        return (a+b);
+    }
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,19 @@
+import com.gsit.MainClass;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class AppTest  {
+
+    @Test
+    public void testResult() throws IOException
+    {
+        assertEquals(new MainClass().addition(10,20),80);
+        System.out.print("Test done");
+    }
+
+}
